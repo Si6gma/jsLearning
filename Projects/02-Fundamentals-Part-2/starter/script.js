07c79e4e47d214fa63593a073b3d8af44d38008a
Si6gma <90695450+Si6gma@users.noreply.github.com>
2022-08-15 15:22:49 +0530

    Before Coding Challenge 3

---
+++ Projects/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,26 @@
+// Array project
+function calcTip(bill) {
+    let tip;
+    if (bill >= 50 && bill <= 300) {
+        tip = (15 / 100) * bill;
+    } else {
+        tip = (20 / 100) * bill;
+    }
+    return tip
+}
+let bills = [125, 555, 44];
+let tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
+let total = [tips[0] + bills[0], tips[1] + bills[1], tips[2] + bills[2]]
+console.log(bills, tips, total);
+
+// Object
+const neiv = {
+    firstName: `Neiv`,
+    lastName: `Malaviya`,
+    birthyear: 2006,
+    age: 2022 - this.birthyear,
+    job: `N/A`,
+    friends: [`Vishaal`, `Riva`, `Pulakit`, `Devesh`],
+}
+
+// Coding Challenge 3.
